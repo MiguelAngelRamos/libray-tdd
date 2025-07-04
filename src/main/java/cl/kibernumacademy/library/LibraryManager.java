6b32a1f4fc76afe2d335309bb80e779cb985dd76
Miguel Ramos <return.infox@gmail.com>
2025-07-03 20:24:10 -0400

    zona verde

--- src/main/java/cl/kibernumacademy/library/LibraryManager.java
+++ src/main/java/cl/kibernumacademy/library/LibraryManager.java
@@ -2,4 +2,8 @@ package cl.kibernumacademy.library;
 
 public class LibraryManager {
   
+  public Book addBook(String title, String author, int year) {
+    return new Book(title, author, year);
+  }
+
 }
